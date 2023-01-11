fd91e560456ece9e3f50d94dd57963ffe5d77798
BrinLewis <brinsley22@hotmail.co.uk>
2023-01-11 12:54:26 +0000

    Create simple rough module and factory

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// Store gameboard as array inside of a Gameboard object.
+// Players will be stored in objects
+// Will need object to control the flow of the game itself
+
+(function gameboardModule() {
+  const gameboard = {
+    board: ['X', 'X', 'O', 'O', 'O', 'X', 'X', 'X', 'O',],
+  };
+})();
+
+function player(name) {
+  return {};
+};
