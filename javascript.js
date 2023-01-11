fa2b8418ac75aa2d3d320e9ec1d48fe9f2909b5a
BrinLewis <brinsley22@hotmail.co.uk>
2023-01-11 15:33:29 +0000

    create function to render markers from array to browser

--- javascript.js
+++ javascript.js
@@ -2,12 +2,22 @@
 // Players will be stored in objects
 // Will need object to control the flow of the game itself
 
-(function gameboardModule() {
+const gameboardModule = (function () {
   const gameboard = {
-    board: ['X', 'X', 'O', 'O', 'O', 'X', 'X', 'X', 'O',],
+    board: ["X", "X", "O", "O", "O", "X", "X", "X", "O"],
   };
+
+  function renderBoard() {
+    // For each item of the array, make the text content of its html counterpart the same as the index value
+    const markers = gameboard.board;
+    markers.forEach((marker, index) => {
+      let boardSegment = document.getElementById(`segment${index + 1}`);
+      boardSegment.textContent = marker;
+    });
+  }
+  renderBoard();
 })();
 
 function player(name) {
   return {};
-};
+}
