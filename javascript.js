9ae391c4445c3a6a068d728d8b31d34958032b79
BrinLewis <brinsley22@hotmail.co.uk>
2023-01-12 13:06:24 +0000

    Code marker placing and player switch after each turn

--- javascript.js
+++ javascript.js
@@ -4,12 +4,12 @@
 
 const gameboardModule = (function () {
   const gameboard = {
-    board: ["X", "X", "O", "O", "O", "X", "X", "X", "O"],
+    board: [" ", " ", " ", " ", " ", " ", " ", " ", " "],
   };
+  const markers = gameboard.board;
 
   function renderBoard() {
     // For each item of the array, make the text content of its html counterpart the same as the index value
-    const markers = gameboard.board;
     markers.forEach((marker, index) => {
       let boardSegment = document.getElementById(`segment${index + 1}`);
       boardSegment.textContent = marker;
@@ -17,27 +17,59 @@ const gameboardModule = (function () {
   }
   renderBoard();
 
-  function makePlay(player, segment) {
+  for (let i = 0; i < 9; i++) {
+    let segment = document.getElementById(`segment${i + 1}`);
+    segment.addEventListener("click", () => {
+      makePlay(segment);
+    });
+  }
 
+  function makePlay(segment) {
+    if (segment.textContent === " ") {
+      let playerMarker = playersModule.getMarker();
+      segment.textContent = playerMarker;
+
+      const arrayIndex = (Number((segment.id).replace("segment", ""))) - 1;
+      markers.splice(arrayIndex, 1, playerMarker);
+
+      playersModule.switchPlayers();
+    } else {
+      alert("This place is already taken, try another!");
+    }
+  }
+  return {
+    markers,
   }
 })();
-
 
 const playersModule = (function () {
   function createPlayer(name, marker) {
     return {
       name,
       marker,
+    };
+  }
+
+  const player1 = createPlayer("mark", "X");
+  const player2 = createPlayer("john", "O");
+  let currentPlayer = player1; //default
+
+  function switchPlayers() {
+    if (currentPlayer === player1) {
+      currentPlayer = player2;
+    } else {
+      currentPlayer = player1;
     }
   }
-  
-  const mark = createPlayer("mark", "X");
-  const john = createPlayer("john", "O");
+
+  function getMarker() {
+    return currentPlayer.marker;
+  }
 
   return {
-    mark,
-    john,
-  }
+    getMarker,
+    switchPlayers,
+  };
 })();
 
 /*makePlay should go in gameModule but be accessible by the player objects so I 
@@ -45,16 +77,19 @@ can call player.makePlay() */
 /* makePlay needs to get a reference to which board segment was clicked, then 
 change the text content of that segment to whatever the players marker is */
 
+/* getPlayer should define the current player as a variable who's value changes
+each time a play is made. */
+
 /* clickHandler function to handle event listeners on the segments. On click 
-it should get the player who clicked it and create a variable referencing which
-segment was clicked, then run the makePlay function, using the player and segment
-as parameters*/
+it should create a variable referencing which segment was clicked, then run the 
+makePlay function with the reference as a parameter */
 
 /* 
 What Each Module Does
   gameboardModule
     -gameboard array
     -render function to display the array on the browser in correct places
+    -click handler loop for event listeners on segments
     -makePlay function to update the array with the playmaker's marker and render it on browser
   playersModule
     -createPlayer function to create a player object with name, marker and shared makePlay method
